232e55825f5935b76c66911f8558c2cc59a6cab8
EthanShen <ethanshen4work@gmail.com>
2024-12-27 17:24:37 +0800

    add basic config koa backend cli

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true, // 句末加分号
+  singleQuote: true, // 用单引号
+  useTabs: true, // 是否使用tab进行缩进（默认false）
+  bracketSpacing: true, // 对象&数组是否追加空格
+  indent: [0, 2],
+  // 避免修改使用 const 声明的变量
+  'no-const-assign': 2,
+  endOfLine: 'auto',
+};
